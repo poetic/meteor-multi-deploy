041dda59fcfa894afcf9535f8dfbbbb740261290
Chun Yang <yang2007chun@gmail.com>
2016-08-04 11:05:24 -0500

    feat: warn user about keystore

--- bin/mmd-setup.js
+++ bin/mmd-setup.js
@@ -5,8 +5,9 @@ const program = require('commander');
 const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
+const { test } = require('../lib/shell');
 
-function generateConfigJson (name) {
+function generateConfigJson(name) {
   const fileName = 'meteor-multi-deploy.json';
   const filePath = path.resolve(fileName);
   const fileContent = `\
@@ -28,7 +29,7 @@ function generateConfigJson (name) {
   "android": {
     "default": {
       "storepass": "<storepass>",
-      "keystorePath": "keystore",
+      "keystorePath": ".keystore",
       "apkOutputPath": "~/Downloads/${name}.apk"
     }
   },
@@ -44,9 +45,44 @@ function generateConfigJson (name) {
 
 program
   .arguments('[name]')
+  .option('-f, --force', 'Replace current json file.')
   .action((name) => {
-    generateConfigJson(name);
-    // TODO: check if keystore exist and let the user change the store pass
+    // check if meteor-multi-deploy.json already exist
+    const jsonExist = test('-f', path.resolve('meteor-multi-deploy.json'));
+    if (jsonExist && !program.force) {
+      console.log(chalk.red(
+        'WARNING: meteor-multi-deploy.json already exist, use -f to force update.'
+      ));
+    } else {
+      generateConfigJson(name);
+    }
+
+    // android
+    // android-keystore
+    const keystoreExist = test('-f', path.resolve('keystore'));
+    if (!keystoreExist) {
+      const keystoreOptionsString = [
+        '-genkey',
+        `-alias ${name}`,
+        '-keyalg RSA',
+        '-keysize 2048',
+        '-validity 10000',
+        '-dname "CN=Matthew Hager, OU=Poeticsystems, O=Poeticsystems, L=Houston, ST=Texas, C=US"',
+      ].join(' ');
+      const keystoreCommand = chalk.green(`keytool ${keystoreOptionsString}`);
+      const helpForKeystore = `
+${chalk.red('WARNING: Andoird keystore file')}
+It seems you do not have a keystore file for android.
+If you have one, please rename it to '.keystore'.
+Otherwise, you can:
+1. run the following command to generate .keystore at ~/.keystore.
+    ${keystoreCommand}
+2. move the .keystore file in your local directory.
+    ${chalk.green('mv ~/.keystore ./')}
+3. replace "<storepass>" in meteor-multi-deploy.json with the correct one.
+`;
+      console.log(helpForKeystore);
+    }
   });
 
 program.on('--help', () => {
